6aa4be5cf3ae274241c282a81ae976136dec88a9
Swayam <sswayam3@gmail.com>
2025-08-28 14:39:31 +0530

    Backend and DB deployed

--- Backend/src/main/java/com/ff/fantasy_football/FantasyFootballApplication.java
+++ Backend/src/main/java/com/ff/fantasy_football/FantasyFootballApplication.java
@@ -1,6 +1,5 @@
 package com.ff.fantasy_football;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,10 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FantasyFootballApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(FantasyFootballApplication.class, args);
 	}
 
